a523ee9a3bba52606fb7f16c08928d75d8f10064
Mohammed Basyouni <mohmah345@gmail.com>
2022-03-26 03:38:56 +0200

    haandling server crash

--- src/utils/products/createProduct.ts
+++ src/utils/products/createProduct.ts
@@ -5,7 +5,13 @@ export async function createProduct(req: Request, res: Response) {
   const name = req.body.name
   const price = parseInt(req.body.price)
 
-  const data = await ProductStore.create({ name, price })
+  try {
+    const data = await ProductStore.create({ name, price })
 
-  res.status(200).json(data)
+    res.status(200).json(data)
+  } catch {
+    res
+      .status(400)
+      .send('please provide a proper product JSON object to create a product')
+  }
 }
